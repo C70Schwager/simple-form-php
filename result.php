928546fe0b8f06da46ed83d9557f21c2603ab081
C70Schwager <cyrillgoesweblop@protonmail.com>
2023-07-10 10:02:17 +0200

    bbb

--- result.php
+++ result.php
@@ -10,6 +10,10 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+  <link rel="stylesheet" href="../../assets/css/style.css">
+
+<title>PHP Intro | Forms in PHP</title>
 </head>
 
 <body>
